8bbd0fb5bfc496a753a82d3569d946142cec4c5a
Andy Bui <andy.bui2001@gmail.com>
2020-08-23 16:29:28 +1000

    add terminal scrolling functionality

--- kernel.c
+++ kernel.c
@@ -32,18 +32,15 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
  
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
-{
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
 	return fg | bg << 4;
 }
  
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
-{
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
  
-size_t strlen(const char* str) 
-{
+size_t strlen(const char* str) {
 	size_t len = 0;
 	while (str[len])
 		len++;
@@ -51,15 +48,25 @@ size_t strlen(const char* str)
 }
  
 static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+static const size_t VGA_HEIGHT = 5;
  
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
+bool terminal_overwrite;
  
-void terminal_initialize(void) 
-{
+void terminal_input_line() {
+	const char* data = ">_";
+	size_t size = strlen(data);
+	for(size_t i = 0; i < size; i++) {
+		const size_t index = (terminal_row) * VGA_WIDTH + (i + terminal_column);
+		terminal_buffer[index] = vga_entry(data[i], terminal_color);
+	}
+}
+
+void terminal_initialize(void) {
+	terminal_overwrite = false;
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
@@ -70,20 +77,53 @@ void terminal_initialize(void)
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
+	terminal_input_line();
 }
  
-void terminal_setcolor(uint8_t color) 
-{
+void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
+
+void terminal_clearline(size_t y) {
+	for(size_t i = 0; i < VGA_WIDTH; i++) {
+		const size_t index = y * VGA_WIDTH + i;
+		terminal_buffer[index] = vga_entry(' ', terminal_color);
+	}
+}
+
+void terminal_scroll(size_t row) {
+
+	// If next row is the limit
+	if (++row >= VGA_HEIGHT - 1) {
+
+		// Shift all lines up if overwriting
+		if (terminal_overwrite) {
+			terminal_row = VGA_HEIGHT - 2;
+
+			for (size_t y = 0; y < VGA_HEIGHT-2; y++) {
+				for (size_t x = 0; x < VGA_WIDTH; x++) {
+					const size_t index = y * VGA_WIDTH + x;
+					const size_t next = index + VGA_WIDTH;
+					terminal_buffer[index] = terminal_buffer[next];
+				}
+			}
+		} else {
+			terminal_overwrite = true;
+			terminal_row = row;
+		}
+	} else {
+		terminal_row = row;
+	}
+
+	// Clear next line for input
+	terminal_clearline(terminal_row);
+}
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	// Check if newline character
 	if (c == '\n') {
-		// TODO: Check if end of terminal
-		terminal_row++;
 		terminal_column = -1;
+		terminal_scroll(terminal_row);
 		return;
 	}
 
@@ -91,33 +131,31 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = vga_entry(c, color);
 }
  
-void terminal_putchar(char c) 
-{
+void terminal_putchar(char c) {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+		terminal_scroll(terminal_row);
 	}
 }
  
-void terminal_write(const char* data, size_t size) 
-{
+void terminal_write(const char* data, size_t size) {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
  
-void terminal_writestring(const char* data) 
-{
+void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
+	terminal_input_line();
 }
- 
-void kernel_main(void) 
-{
+
+void kernel_main(void) {
 	/* Initialize terminal interface */
 	terminal_initialize();
-	
+
 	/* Write string to terminal */
-	terminal_writestring("Hello, kernel World!\n");
-	terminal_writestring("I was created by Andy Bui\nlegend\n");
+	terminal_writestring("Zero\n");
+	terminal_writestring("One\n");
+	terminal_writestring("Two\n");
+	terminal_writestring("Three\n");
 }
