b1b056b404a5b59b7b8a87b24294d6283696af6c
Andy Bui <andy.bui2001@gmail.com>
2020-09-08 12:28:08 +1000

    remove test dir

--- test/gdt.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KERNEL_GDT_H
-#define _KERNEL_GDT_H
-
-#include <stdint.h>
-
-void gdt_bootstrap();
-void gdt_create_desc(int num, uint32_t base, uint32_t limit, uint16_t access, uint8_t gran);
-
-#endif
