2723844916b0cab26fce1ac6dd9d1b537eb30043
andybui01 <andy.bui2001@gmail.com>
2020-10-18 14:04:43 +1100

    bad attempt at higher half kernel, triple faulting

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,22 +10,33 @@
 #include <stdint.h>
 #include <debug.h>
 
+extern uint32_t _kernel_end;
+
 void kernel_main(multiboot_info_t *mbt)
 {
 
     // initialize terminal
     terminal_initialize();
+    printf("hi\n");
+
+
+    // mem_bootstrap(mbt);
 
     // initialize GDT
     gdt_bootstrap();
 
     // initialize IDT
     idt_bootstrap();
+    
+    kbreak();
+    // printf("0x%x\n", mbt->mmap_addr);
+    printf("kernel ends at: 0x%x\n", _kernel_end);
+    int a = 5/0;
+    (void) a;
+
 
     // remap PIC
     pic_remap(0x20, 0x28);
-
-    mem_bootstrap(mbt);
 
     // infinite loop ya!
     for (;;) {
--- kernel/mem/bootstrap.c
+++ kernel/mem/bootstrap.c
@@ -4,5 +4,6 @@
 
 void mem_bootstrap(multiboot_info_t *mbt)
 {
+    printf("addr: 0x%x len: 0x%x\n", mbt->mmap_addr, mbt->mmap_length);
     pmm_bootstrap(mbt->mmap_addr, mbt->mmap_length);
 }
