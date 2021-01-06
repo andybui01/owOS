ec01a3256ba9fe00d435f1a65761e5fa95ef617e
Andy Bui <andy.bui2001@gmail.com>
2021-01-06 16:08:46 +1100

    Refactor GDT code

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,34 +1,77 @@
 #include <kernel/gdt.h>
+#include <kernel/tss.h>
+
 #include <stdint.h>
 #include <stdio.h>
 
 // global descriptor table
-gdt_entry_t gdt[3];
+gdt_entry_t gdt[GDT_ENTRIES];
 gdt_ptr_t gp;
 
 void gdt_bootstrap()
 {
 
     // Set up GDT pointer and limit
-    gp.limit = (sizeof(gdt_entry_t) * 3) - 1;
+    gp.limit = (sizeof(gdt_entry_t) * GDT_ENTRIES) - 1;
     gp.base = (uintptr_t) &gdt;
 
     // NULL descriptor
-    gdt_create_desc(0, 0, 0, 0, 0);
+    gdt_create_desc(GDT_INDEX_NULL, 0, 0, 0, 0);
 
     // CODE segment descriptor
-    gdt_create_desc(1, 0, 0xFFFFFFFF, 0x9A, 0xC);
+    gdt_create_desc(GDT_INDEX_CS0, 0, 0xFFFFFFFF,   (
+                                                    GDT_ACCESS_PRESENT |
+                                                    GDT_ACCESS_DPL(0) |
+                                                    GDT_ACCESS_TYPE |
+                                                    GDT_ACCESS_EXEC |
+                                                    GDT_ACCESS_RW
+                                                    )
+                                                ,   (
+                                                    GDT_FLAGS_GRAN |
+                                                    GDT_FLAGS_SIZE
+                                                    )
+                                                );
 
     // DATA segment descriptor
-    gdt_create_desc(2, 0, 0xFFFFFFFF, 0x92, 0xC);
-
+    gdt_create_desc(GDT_INDEX_DS0, 0, 0xFFFFFFFF,   (
+                                                    GDT_ACCESS_PRESENT |
+                                                    GDT_ACCESS_DPL(0) |
+                                                    GDT_ACCESS_TYPE |
+                                                    GDT_ACCESS_RW
+                                                    )
+                                                ,   (
+                                                    GDT_FLAGS_GRAN |
+                                                    GDT_FLAGS_SIZE
+                                                    )
+                                                );
     // USER CODE
-    gdt_create_desc(3, 0, 0xFFFFFFFF, 0xFA, 0xC);
+    gdt_create_desc(GDT_INDEX_CS3, 0, 0xFFFFFFFF,   (
+                                                    GDT_ACCESS_PRESENT |
+                                                    GDT_ACCESS_DPL(3) |
+                                                    GDT_ACCESS_TYPE |
+                                                    GDT_ACCESS_EXEC |
+                                                    GDT_ACCESS_RW
+                                                    )
+                                                ,   (
+                                                    GDT_FLAGS_GRAN |
+                                                    GDT_FLAGS_SIZE
+                                                    )
+                                                );
 
     // USER DATA
-    gdt_create_desc(4, 0, 0xFFFFFFFF, 0xF2, 0xC);
+    gdt_create_desc(GDT_INDEX_DS3, 0, 0xFFFFFFFF,   (
+                                                    GDT_ACCESS_PRESENT |
+                                                    GDT_ACCESS_DPL(3) |
+                                                    GDT_ACCESS_TYPE |
+                                                    GDT_ACCESS_RW
+                                                    )
+                                                ,   (
+                                                    GDT_FLAGS_GRAN |
+                                                    GDT_FLAGS_SIZE
+                                                    )
+                                                );
 
-    tss_bootstrap();
+    // tss_bootstrap();
 
     // Flush old GDT (GRUB) and implement new one
     gdt_flush((uintptr_t) &gp);
@@ -36,7 +79,7 @@ void gdt_bootstrap()
 }
 
 // Create a GDT descriptor
-void gdt_create_desc(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
+void gdt_create_desc(unsigned int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
 {
     // Set the descriptor base address
     gdt[num].base_low = (base & 0xFFFF);
@@ -47,7 +90,15 @@ void gdt_create_desc(int num, uint32_t base, uint32_t limit, uint8_t access, uin
     gdt[num].limit_low = (limit & 0xFFFF);
     gdt[num].limit_high = ((limit >> 16) & 0xF);
 
-    /* Finally, set up the granularity and access flags */
+    // Granularity and size bit ALWAYS set unless NULL descriptor
     gdt[num].flags = (flags & 0xF);
     gdt[num].access = access;
+}
+
+static void tss_bootstrap(uint32_t num, uint16_t ss0, uint32_t esp0)
+{
+    (void) num;
+    (void) ss0;
+    (void) esp0;
+    return;
 }
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -1,7 +1,10 @@
 #ifndef _KERNEL_GDT_H
 #define _KERNEL_GDT_H
 
+#ifndef __ASSEMBLER__
 #include <stdint.h>
+
+#define GDT_ENTRIES 5
 
 // We define these variables in this order so that for the entire GDT entry
 // all the bits are next to each other in memory
@@ -17,9 +20,22 @@ struct gdt_entry {
 	uint8_t flags :4;
 
     uint8_t base_high;
-}__attribute__((packed));
+} __attribute__((packed));
 
 typedef struct gdt_entry gdt_entry_t;
+
+// For full definition of bit masks, goto
+// https://wiki.osdev.org/Global_Descriptor_Table
+#define GDT_FLAGS_SIZE 0x4
+#define GDT_FLAGS_GRAN 0x8
+
+#define GDT_ACCESS_ACCESS   0x01
+#define GDT_ACCESS_RW       0x02
+#define GDT_ACCESS_DC       0x04
+#define GDT_ACCESS_EXEC     0x08
+#define GDT_ACCESS_TYPE     0x10
+#define GDT_ACCESS_DPL(x)   (x << 5)
+#define GDT_ACCESS_PRESENT  0x80
 
 // Special pointer which includes the limit:
 // max bytes taken up by GDT minus 1
@@ -34,8 +50,24 @@ typedef struct gdt_ptr gdt_ptr_t;
 extern void gdt_flush(uintptr_t);
 
 void gdt_bootstrap();
-void gdt_create_desc(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
+void gdt_create_desc(unsigned int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
 
-void tss_bootstrap(void);
+#endif
+
+// 0 = kernel privilege, 3 = user privilege
+#define GDT_INDEX_NULL  0
+#define GDT_INDEX_CS0   1
+#define GDT_INDEX_DS0   2
+#define GDT_INDEX_CS3   3
+#define GDT_INDEX_DS3   4
+#define GDT_INDEX_TSS   5
+
+#define TABLE_GDT_MASK 0x0
+#define TABLE_LDT_MASK 0x4
+
+#define RPL_KERNEL  0x0
+#define RPL_USER    0x3
+
+#define SEGMENT_SELECTOR_INDEX(x) (x << 3)
 
 #endif
